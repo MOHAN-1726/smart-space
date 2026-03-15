d334fb4911982ec08095e88cc887d2ee082feddc
MOHAN-1726 <mohanraj.k1729@gmail.com>
2026-03-15 13:30:02 +0530

    Fix package.json merge conflict

--- src/routes/message.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getMessagesByClass, createMessage } from '../controllers/message';
-
-const router = Router();
-
-router.get('/:classId', getMessagesByClass);
-router.post('/', createMessage);
-
-export default router;
