ebcf781d0997ecc225185c59cd187df743987881
MOHAN-1726 <mohanraj.k1729@gmail.com>
2026-03-16 13:05:26 +0530

    Clear data

--- backend/scripts/seedDatabase.js
+++
@@ -1,9 +0,0 @@
-import { initDatabase } from '../database.js';
-
-async function seedDatabase() {
-    await initDatabase();
-    console.log('SQLite database initialized and seeded');
-    process.exit(0);
-}
-
-seedDatabase();
